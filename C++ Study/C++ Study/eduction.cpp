0c36b834c43a1a3cb7e1a065ea3da16a62caea7f
DESKTOP-V10EKSP\user <coco1011@kakao.com>
2024-02-21 08:52:05 +0900

    add

--- C++ Study/C++ Study/eduction.cpp
+++ C++ Study/C++ Study/eduction.cpp
@@ -1,6 +1,46 @@
 //이공간은 학원 전용입니다.
+#include<iostream>
+
+using namespace std;
 
 int main()
 {
+	// 자료형 (크기 단위, byte) Data Type 
+	// 정수형 : char(1), short(2), int(4), long(4), long long(8)
+	// 실수형 : float(4), double(8)
+
+	int i = 0;
+
+	// 1 바이트로 양수만 표현
+	// 256 가지 -> 0 ~ 255
+	unsigned char c = 0;
+
+	c = 0;
+	c = 255;
+
+
+	// 1바이트로 양수, 음수 둘다 표현
+	// -128 ~ 0 ~127
+	char c1 = 0;
+	c1 = 1;
+	// 자료형 (크기 단위, byte) Data Type 
+		// 정수형 : char(1), short(2), int(4), long(4), long long(8)
+		// 실수형 : float(4), double(8)
+
+	int i = 0;
+
+	// 1 바이트로 양수만 표현
+	// 256 가지 -> 0 ~ 255
+	unsigned char c = 0;
+
+	c = 0;
+	c = 255;
+
+
+	// 1바이트로 양수, 음수 둘다 표현
+	// -128 ~ 0 ~127
+	char c1 = 0;
+	c1 = 1;
+
 	return 0;
 }
