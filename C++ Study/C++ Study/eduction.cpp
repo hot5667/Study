055d8c73e6abda7cd7aeb4550f419690df611165
DESKTOP-V10EKSP\user <coco1011@kakao.com>
2024-02-21 23:27:47 +0900

    상중비교 연산자 Ex

--- C++ Study/C++ Study/eduction.cpp
+++ C++ Study/C++ Study/eduction.cpp
@@ -2,7 +2,6 @@
 #include<iostream>
 
 using namespace std;
-
 int main()
 {
 	// 자료형 (크기 단위, byte) Data Type 
@@ -48,6 +47,53 @@ int main()
 	//if 문법
 
 	int data = 0;
+
+	//비교 연산자 
+	// == != < <= > >= 
+
+	//삼중 비교 연산자
+	//<=>
+
+	//Ex 
+
+	//class T {
+
+	//	int m_Val;
+
+	//public:
+	//	explicit T(int val) : m_Val{ val } {}
+	//	bool operator ==(const T& other) const { return m_Val == other.m_Val; } // < 로부터 구현하면, !(*this < other || other < *this)로 할 수 있습니다. 단 < 을 2회 하므로 비효율적입니다.
+	//	bool operator !=(const T& other) const { return !(*this == other); } // == 로부터 구현
+	//	bool operator <(const T& other) const { return m_Val < other.m_Val; }
+	//	bool operator >(const T& other) const { return other < *this; } // < 로부터 구현
+	//	bool operator <=(const T& other) const { return !(other < *this); } // < 로부터 구현
+	//	bool operator >=(const T& other) const { return !(*this < other); } // < 로부터 구현
+	//};
+
+	//EXPECT_TRUE(T{ 10 } == T{ 10 });
+	//EXPECT_TRUE(T{ 10 } != T{ 20 });
+	//EXPECT_TRUE(T{ 10 } < T{ 20 });
+	//EXPECT_TRUE(T{ 20 } > T{ 10 });
+	//EXPECT_TRUE(T{ 10 } <= T{ 20 } && T{ 10 } <= T{ 10 });
+	//EXPECT_TRUE(T{ 20 } >= T{ 10 } && T{ 10 } >= T{ 10 });
+
+	/*-----------------------------------------------------------------------------------------------------------------*/
+
+	//class T_20 {
+	//	int m_Val;
+	//public:
+	//	explicit T_20(int val) : m_Val{ val } {}
+
+	//	std::strong_ordering operator <=>(const T_20& other) const { return m_Val <=> other.m_Val; }
+	//	bool operator ==(const T_20& other) const { return m_Val == other.m_Val; }
+	//};
+
+	//EXPECT_TRUE(T_20{ 10 } == T_20{ 10 });
+	//EXPECT_TRUE(T_20{ 10 } != T_20{ 20 });
+	//EXPECT_TRUE(T_20{ 10 } < T_20{ 20 });
+	//EXPECT_TRUE(T_20{ 20 } > T_20{ 10 });
+	//EXPECT_TRUE(T_20{ 10 } <= T_20{ 20 } && T_20{ 10 } <= T_20{ 10 });
+	//EXPECT_TRUE(T_20{ 20 } >= T_20{ 10 } && T_20{ 10 } >= T_20{ 10 });
 
 	if (0 && 200) //거짓
 	{
